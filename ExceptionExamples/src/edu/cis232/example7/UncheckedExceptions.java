d036b670c240f5d4c58ff6bebd776502e2bfbb4c
SK207_inst <SK207_inst@K207_W8Inst.carrollcc.edu>
2016-02-29 20:35:09 -0500

    Unchecked/RuntimeExceptions

--- ExceptionExamples/src/edu/cis232/example7/UncheckedExceptions.java
+++ ExceptionExamples/src/edu/cis232/example7/UncheckedExceptions.java
@@ -2,9 +2,23 @@ package edu.cis232.example7;
 
 public class UncheckedExceptions {
 
+	String name;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		UncheckedExceptions program = new UncheckedExceptions();
+		try{
+			program.run();
+		} catch (NullPointerException ex){
+			System.out.println("Error! The `name` field was never set.");
+		} catch (IndexOutOfBoundsException ex){
+			System.out.println("Tried to access a character index that does not exist. Index goes from 0 to length -1");
+		}
+	}
+	
+	public void run(){
+		name = "Bob";
+		System.out.println(name.length());
+		name.charAt(name.length());
 	}
 
 }
