a1ffa4f8fb423088a3ebe0e3375afddd0fda975c
unknown <SK207_inst@K207_W8Inst.carrollcc.edu>
2016-02-29 20:23:31 -0500

    Catch multiple exception types in one block

--- ExceptionExamples/src/edu/cis232/example6/HandleMultipleExceptionsInOneBlock.java
+++ ExceptionExamples/src/edu/cis232/example6/HandleMultipleExceptionsInOneBlock.java
@@ -1,9 +1,18 @@
 package edu.cis232.example6;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class HandleMultipleExceptionsInOneBlock {
 
 	public static void main(String[] args) {
 		
+		try{
+			Scanner input = new Scanner(new File("bad.txt"));
+		} catch (FileNotFoundException | IllegalArgumentException ex){
+			System.out.println("The file does not exist");
+		}
 
 	}
 
