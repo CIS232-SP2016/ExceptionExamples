7ceba2964cbc088e4a168b84505082ed5d4fb0b2
SK207_inst <SK207_inst@K207_W8Inst.carrollcc.edu>
2016-02-29 19:49:23 -0500

    First try/catch in example 3

--- ExceptionExamples/src/edu/cis232/example2/ExceptionExample2.java
+++ ExceptionExamples/src/edu/cis232/example2/ExceptionExample2.java
@@ -4,6 +4,8 @@ public class ExceptionExample2 {
 
 	public static void main(String[] args) throws Exception {
 		methodOne();
+		
+		System.out.println("The program continues!");
 	}
 
 	public static void methodOne() throws Exception{
---
+++ ExceptionExamples/src/edu/cis232/example3/ExceptionExample3.java
@@ -0,0 +1,26 @@
+package edu.cis232.example3;
+
+import java.util.concurrent.TimeoutException;
+
+public class ExceptionExample3 {
+
+	public static void main(String[] args){
+		try {
+			methodOne();
+		} catch (Exception ex){
+			//You should print this exception to a log file, but we are not
+			//doing that here.
+			
+			//Show an error message (user friendly)
+			System.out.println(ex.getMessage());
+			
+		}
+		
+		System.out.println("The program continues...");
+	}
+	
+	public static void methodOne() throws Exception{
+		throw new TimeoutException("Time out!");
+	}
+
+}
