24f64f57bb145728c01ee5949bed5374de09f28a
SK207_inst <SK207_inst@K207_W8Inst.carrollcc.edu>
2016-02-29 19:23:26 -0500

    Example #1. throw an exception

--- ExceptionExamples/src/edu/cis232/ExceptionExample1.java
+++ ExceptionExamples/src/edu/cis232/ExceptionExample1.java
@@ -2,9 +2,8 @@ package edu.cis232;
 
 public class ExceptionExample1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		throw new Exception("This is an exception");
 	}
 
 }
