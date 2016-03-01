d2fa02d3f1c3c1d2ce28a3cf2b93f425dd656e76
unknown <SK207_inst@K207_W8Inst.carrollcc.edu>
2016-02-29 20:12:26 -0500

    handle multiple exceptions

---
+++ ExceptionExamples/src/edu/cis232/example5/CatchMultipleExceptions.java
@@ -0,0 +1,33 @@
+package edu.cis232.example5;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class CatchMultipleExceptions {
+
+	public static void main(String[] args) {
+		try {
+			Scanner keyboard = new Scanner(System.in);
+			System.out.println("What is the name of the numbers file?");
+			File file = new File(keyboard.nextLine());
+			Scanner input = new Scanner(file);
+			int total = 0;
+			while(input.hasNextLine()){
+				total += Integer.parseInt(input.nextLine());
+			}
+			System.out.printf("The total is %d%n", total);
+			input.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("The file was not found!");
+		} catch (NumberFormatException e){
+			System.out.printf("There was an error while reading the file: %s%n", e.getMessage());
+		} catch (Exception e){
+			System.out.println("Some error happened!");
+			e.printStackTrace();
+		}
+	}
+	
+	
+
+}
